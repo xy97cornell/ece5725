9ffa4d5e22c817e26fb7e7e658aab62e99845837
Xiaoyu <xy97@cornell.edu>
2018-12-06 01:28:06 -0500

    -a

--- final_project/acc_send.py
+++ final_project/acc_send.py
@@ -1,9 +1,25 @@
 import accelerometer
 import time
 import socket
+import RPi.GPIO as GPIO
 
 client_IP = '10.148.0.210'
 PORT1 = 5000
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+turn = 0
+
+def GPIO22_callback(channel):
+    """
+    interrupt handler for GPIO17; button on piTFT
+    """
+    print ("in interrupt 22")
+    global turn 
+    turn = 1 if 0 else 1
+
+
+GPIO.add_event_detect(22, GPIO.FALLING, callback=GPIO22_callback, bouncetime=300)
 
 code_running = True
 if __name__=='__main__':
@@ -14,7 +30,7 @@ if __name__=='__main__':
 			while True:
 				h,r,p = accelerometer.bno_poll()
 				print ("r = ",r)
-				message = str(h)+":"+str(r)+":"+str(p)
+				message = str(h)+":"+str(r)+":"+str(p)+":"+str(turn)
 				command_server.sendto(message.encode(), (client_IP, PORT1))
 				time.sleep(0.9)
 				
